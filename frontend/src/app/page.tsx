263e295335ead3ec8bd0529bb198276c243c36bc
Krish-Parekh <krishparekh303@gmail.com>
2025-03-07 10:51:45 +0530

    feat: initialize the code

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">
+        Hello World Cloud Computing test application
+      </h1>
+    </div>
+  );
+}
